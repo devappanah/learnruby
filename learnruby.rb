d789507127db30fe2b3e6c61a72a21b6f01657f3
Dev <devappanah@icloud.com>
2013-11-30 12:17:51 +0700

    delayed upload

--- learnruby.rb
+++ learnruby.rb
@@ -1,7 +1,7 @@
 class Human
-  attr_reader :name, :age, :location
-  attr_writer :name, :age
-  # attr_accessor :name, :age, :location
+  # attr_reader :name, :age, :location
+  # attr_writer :name, :age
+  attr_accessor :name, :age, :location
   def initialize(name, age, location)
     @name = name
     @age = age
@@ -13,4 +13,22 @@ class Human
   # def name=(value)
   # 	@age = value
   # end
+end
+class Engineer < Human
+  def initialize(name, age, location, type)
+    super(name, age, location)
+    @type = type
+  end
+  def job
+    if @type.downcase == 'civil'
+      "Build bridges & buildings"
+    elsif @type.downcase == 'software'
+      "Builds Software"
+    elsif @type.downcase == 'electrical'
+      "Setup electricity"
+    elsif @type.downcase == 'oil'
+      'Dig for oil'
+    end
+  end
+
 end
