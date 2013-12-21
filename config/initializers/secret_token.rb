1fe23c79bb7c166b0f7471e42af0dd858440a0ae
Dev <devappanah@icloud.com>
2013-12-21 13:40:44 +0700

    start bizglobal

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bizglobal::Application.config.secret_key_base = '4d9b64ce2183232554199e645532c686cbf756e6d59d4313bd59c85b10ff6858eb302fe1c781a345655215d15d57ed66503f04a52b048c51dfce2c4fb51042f4'
